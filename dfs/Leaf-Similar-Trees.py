a5fa718c04ec6914e3dced3f3cd790077034af3a
Priya Gupta <priyagupta14317@gmail.com>
2025-04-29 19:20:55 -0400

    [872] [Time Beats: 100.00%] [Memory Beats: 96.05%] - LeetPush

---
+++ dfs/Leaf-Similar-Trees.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: bool
+        """
+    
+        def dfs(root):
+            if root is None:
+                return [] 
+            leaves = dfs(root.left) + dfs(root.right)
+
+            return leaves or [root.val]
+        
+        return dfs(root1) == dfs(root2)
+
+        
