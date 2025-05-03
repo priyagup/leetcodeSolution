c0e5bba74d3d3aa38bb0d7e31cfa802359496514
Priya Gupta <priyagupta14317@gmail.com>
2025-05-03 13:16:09 -0400

    [617] [Time Beats: 100.00%] [Memory Beats: 64.54%] - LeetPush

--- Trees/Merge-Two-Binary-Trees.java
+++ Trees/Merge-Two-Binary-Trees.java
@@ -26,3 +26,36 @@ class Solution {
          return root1;
     }
 }
+/*
+ public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+        Queue<TreeNode[]> queue = new LinkedList<>();
+        queue.offer(new TreeNode[] {root1, root2});
+
+        while (!queue.isEmpty()) {
+            TreeNode[] current = queue.poll();
+            TreeNode t1 = current[0], t2 = current[1];
+
+            // Merge t2 into t1
+            t1.val += t2.val;
+
+            // Handle left child
+            if (t1.left != null && t2.left != null) {
+                queue.offer(new TreeNode[] {t1.left, t2.left});
+            } else if (t1.left == null) {
+                t1.left = t2.left;
+            }
+
+            // Handle right child
+            if (t1.right != null && t2.right != null) {
+                queue.offer(new TreeNode[] {t1.right, t2.right});
+            } else if (t1.right == null) {
+                t1.right = t2.right;
+            }
+        }
+
+        return root1;
+    }
+    */
