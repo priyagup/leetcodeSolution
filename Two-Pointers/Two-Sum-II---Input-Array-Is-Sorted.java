054f6ac9c085c95a0e10189c49c1f330e77b82a8
Priya Gupta <priyagupta14317@gmail.com>
2025-05-13 13:22:39 -0400

    [167] [Time Beats: 93.10%] [Memory Beats: 87.08%] - LeetPush

---
+++ Two-Pointers/Two-Sum-II---Input-Array-Is-Sorted.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left =0; 
+        int right = numbers.length -1; 
+
+
+       while(left < right){
+
+        int num = numbers[left] + numbers[right];
+
+        if(target == num){
+           return new int[] {left+1, right+1};
+            
+        } else if(target > num){
+            left++;
+        }else{
+            right--;
+
+        }
+        
+       }
+
+       return new int[]{-1,-1};
+    }
+}
